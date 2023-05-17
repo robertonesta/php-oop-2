55649fe7f28e246049c5117472405845c40ffb4d
RobertoNesta <robertonesta1993@gmail.com>
2023-05-17 18:07:23 +0200

    add a desktop and a laptop

--- index.php
+++ index.php
@@ -2,18 +2,19 @@
 
 class computer{
 
-    public function __construct(public brand $brand, public RAM $RAM, public CPU $CPU, public memory $memory)
+    public function __construct(public brand $brand, public RAM $RAM, public CPU $CPU, public memory $memory, public display $display)
     {
         $this->brand=$brand;
         $this->RAM=$RAM;
         $this->CPU=$CPU;
         $this->memory=$memory;
+        $this->display=$display;
     }
 
 }
 class brand{
 
-    public function __construct(public string $model, public string $codeNumber)
+    public function __construct(public string $model, public float $codeNumber)
     {
         $this->model=$model;
         $this->codeNumber=$codeNumber;
@@ -21,15 +22,15 @@ class brand{
 }
 class RAM{
 
-    public function __construct(public string $speed, public int $MHz)
+    public function __construct(public string $speed, public float $memory)
     {
         $this->speed=$speed;
-        $this->MHz=$MHz;
+        $this->memory=$memory;
     }
 }
 class CPU{
 
-    public function __construct(public string $core, public string $frequency)
+    public function __construct(public string $core, public float $frequency)
     {
         $this->core=$core;
         $this->frequency=$frequency;
@@ -37,17 +38,26 @@ class CPU{
 }
 class memory{
 
-    public function __construct(public string $type, public string $capacity)
+    public function __construct(public string $capacity)
     {
-        $this->type=$type;
         $this->capacity=$capacity;
     }
 }
 
+class display{
+
+    public function __construct(public string $size, public string $resolution)
+    {
+        $this->size=$size;
+        $this->resolution=$resolution;
+    }
+}
+
+
 class desktop extends computer{
 
-    public function __construct(public brand $brand, public RAM $RAM, public CPU $CPU, public memory $memory, public mouse $mouse, public keyboard $keyboard){
-        parent::__construct($brand, $RAM, $CPU, $memory);
+    public function __construct(public brand $brand, public RAM $RAM, public CPU $CPU, public memory $memory, public display $display, public mouse $mouse, public keyboard $keyboard){
+        parent::__construct($brand, $RAM, $CPU, $memory, $display);
         {
             $this->mouse=$mouse;
             $this->keyboard=$keyboard;
@@ -57,25 +67,25 @@ class desktop extends computer{
 
 class mouse {
 
-    public function __construct(public string $brand, public int $keys)
+    public function __construct(public string $model, public int $keys)
     {
-        $this->brand=$brand;
+        $this->model=$model;
         $this->keys=$keys;
     }
 
 }
 class keyboard {
 
-    public function __construct(public string $brand, public string $model)
+    public function __construct(public string $layout, public string $model)
     {
-        $this->brand=$brand;
+        $this->layout=$layout;
         $this->model=$model;
     }
 }
 class laptop extends computer{
 
-    public function __construct(public brand $brand, public RAM $RAM, public CPU $CPU, public memory $memory, public webcam $webcam, public battery $battery){
-        parent::__construct($brand, $RAM, $CPU, $memory);
+    public function __construct(public brand $brand, public RAM $RAM, public CPU $CPU, public memory $memory, public display $display, public webcam $webcam, public battery $battery){
+        parent::__construct($brand, $RAM, $CPU, $memory, $display);
         {
             $this->webcam=$webcam;
             $this->battery=$battery;
@@ -85,26 +95,23 @@ class laptop extends computer{
 
 class webcam {
 
-    public function __construct(public string $brand, public float $resolution)
+    public function __construct(public string $model, public float $resolution)
     {
-        $this->brand=$brand;
+        $this->model=$model;
         $this->resolution=$resolution;
     }
 
 }
 class battery {
 
-    public function __construct(public string $model, public string $volt, public string $Ampere, public string $watts)
+    public function __construct(public string $batteryLife)
     {
-        $this->model=$model;
-        $this->volt=$volt;
-        $this->Ampere=$Ampere;
-        $this->watts=$watts;
+        $this->batteryLife=$batteryLife;
     }
 }
 
-
-
+$desktopAppleiMac = new desktop (new brand ("Apple iMac","MMQA2xx/A"), new RAM("1600MHz","32GB"), new CPU ("M1 8core", "3GHz"), new memory("512GB"), new display("27inch", "Display Retina 4,5K"), new mouse("magic mouse", 2), new keyboard("american", "Magic Keyboard con Touch ID"));
+$laptopAppleMacbook = new laptop (new brand ("Apple Macbook", "MMQA2xx/A"), new RAM("1600MHz", "16GB"), new CPU("M2 Pro 10core", "3GHz"), new memory("256GB"), new display("14inch", "Display Liquid Retina XDR1"), new webcam("Videocamera FaceTime HD", "1080p"), new battery("18 hrs"));
 
 ?>
 
